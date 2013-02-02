58637d77d3491348b723ef4e9a07478dc3d6fbed
WebbingStudio <mail@webbingstudio.com>
2013-02-03 03:09:12 +0900

    [a-blog cms][1.10] 文字コード・改行コード・インデントを統一

--- ablogcms/import_logical_jp_acms/themes/logical_jp_acms/resources/js/home.js
+++ ablogcms/import_logical_jp_acms/themes/logical_jp_acms/resources/js/home.js
@@ -1,36 +1,36 @@
 /* =======================================
 
-	Home JS
+  Home JS
 
 ========================================== */
 
 (function($){ // start jquery
 
 $(window).resize(function() {
-	resetHeight($('.home #mainVisual'));
+  resetHeight($('.home #mainVisual'));
 });
 
 $(window).load(function() {
-	resetHeight($('.home #mainVisual'));
+  resetHeight($('.home #mainVisual'));
 });
 
 $(function(){
-	/* slideMenu
-	========================================== */
-	var Obj = $('.home #mainVisual');
-	$('.mvSlide a', Obj).each(function(){
-		//hrefが「#」であればリンクを除去する
-		var HREF = $(this).attr('href');
-		if(HREF == '#'){
-			$(this).replaceWith($(this).html());
-		}
-	});
-	Obj.wsSlideMenu();
+  /* slideMenu
+  ========================================== */
+  var Obj = $('.home #mainVisual');
+  $('.mvSlide a', Obj).each(function(){
+    //hrefが「#」であればリンクを除去する
+    var HREF = $(this).attr('href');
+    if(HREF == '#'){
+      $(this).replaceWith($(this).html());
+    }
+  });
+  Obj.wsSlideMenu();
 }); // end $(function
 
 var resetHeight = function(Obj) {
-	var H = $('#mvContainer .mvSlide img', Obj).eq(0).height();
-	$('#mvContainer, .mvNav', Obj).height(H);
+  var H = $('#mvContainer .mvSlide img', Obj).eq(0).height();
+  $('#mvContainer, .mvNav', Obj).height(H);
 }
 
 })(jQuery); // end jquery
